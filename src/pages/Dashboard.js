b23eb1c564163dfe774c5db0516a9fb30547d684
danielgallas <danielgallas2@gmail.com>
2023-02-23 17:31:39 +0000

    enhancement to new entry field - with title

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,6 +5,9 @@ import data from "../data";
 function Dashboard() {
   const [logs, setLogs] = useState(data);
   const [currentEntry, setCurrentEntry] = useState(null);
+  const [newEntry, setNewEntry] = useState("");
+  const [newTitle, setNewTitle] = useState("");
+  const [newId, setNewId] = useState(4);
 
   // const getData = async () => {
   //   try {
@@ -28,8 +31,20 @@ function Dashboard() {
     return;
   };
 
+  const handleEntry = (e, newEntry, newTitle) => {
+    e.preventDefault();
+    setNewId(newId + 1);
+    let newItem = {
+      id: newId,
+      title: newTitle,
+      entry: newEntry,
+      timestamp: "February 23 2022, 17:17",
+    };
+    setLogs([...data, newItem]);
+  };
+
   return (
-    <>
+    <section>
       <div className="dashboard-container dashboard-list">
         <h1>My log: </h1>
         {logs.map((item) => {
@@ -47,7 +62,7 @@ function Dashboard() {
             </div>
           );
         })}
-        <div className="dashboard-item" style={{ "text-align": "center" }}>
+        <div className="dashboard-item">
           <button
             className="dashboard-clickable"
             onClick={() => setCurrentEntry(null)}
@@ -61,10 +76,28 @@ function Dashboard() {
         {currentEntry ? (
           currentEntry
         ) : (
-          <textarea className="dashboard-item"></textarea>
+          <>
+            <form onSubmit={(e) => handleEntry(e, newEntry, newTitle, newId)}>
+              <textarea
+                onChange={(e) => setNewEntry(e.target.value)}
+                className="dashboard-item"
+                rows="10"
+              />
+              <input
+                onChange={(e) => setNewTitle(e.target.value)}
+                className="dashboard-item"
+              />
+              <button
+                className="dashboard-item dashboard-clickable"
+                type="submit"
+              >
+                Submit entry
+              </button>
+            </form>
+          </>
         )}
       </div>
-    </>
+    </section>
   );
 }
 
