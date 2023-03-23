1f2143320a80c701f6f73bf8f835c33272e80580
Daniel Gallas <danielgallas2@gmail.com>
2023-03-23 09:03:30 +0000

    add netlify redirects to client build, change port to env in server

--- server/server.js
+++ server/server.js
@@ -7,20 +7,22 @@ const authRouter = require("./routes/auth");
 const tasksRouter = require("./routes/tasks");
 const authenticateUser = require("./middleware/authentication");
 
+const port = process.env.PORT || 5000;
+
 // middleware
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "https://my-log.netlify.app" }));
 app.use(express.json());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/tasks", tasksRouter);
+app.use("/api/v1/tasks", authenticateUser, tasksRouter);
 
 // app.use("/api/v1/tasks", authenticateUser, tasksRouter);
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(5000, () => {
+    app.listen(port, () => {
       console.log(
         "Server is running on port 5000 and connected to the database"
       );
